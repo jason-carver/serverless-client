aa2146726920ea0537a2a6997b335d1ee8666e9f
Jason Carver <jbrittcarver@hotmail.com>
2020-03-03 20:34:49 -0600

    setting up react frontend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "bucketonotes"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://fx1w7oolz1.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_KQesvlsjM",
+      APP_CLIENT_ID: "6rs6r0rsuk7uq6npbdnbuqncue",
+      IDENTITY_POOL_ID: "us-east-1:d84a5697-1c00-4908-9434-16e4e11ff5dd"
+    }
+  };
