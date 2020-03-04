2c44f3ca4d0f5cf8c31289e12bcf9e065a3b7592
Jason Carver <jbrittcarver@hotmail.com>
2020-03-04 15:53:12 -0600

    completed React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+   MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-1",
       BUCKET: "bucketonotes"
@@ -12,5 +13,6 @@ export default {
       USER_POOL_ID: "us-east-2_KQesvlsjM",
       APP_CLIENT_ID: "6rs6r0rsuk7uq6npbdnbuqncue",
       IDENTITY_POOL_ID: "us-east-1:d84a5697-1c00-4908-9434-16e4e11ff5dd"
-    }
+    },
+    STRIPE_KEY: "pk_test_n1sBjALNl7edw^zcbsrYf7GS00UG7GhBaN"  
   };
