5ab95a37b2573a973e0334b412a66e4394878ba0
“Jason”-carver <jbrittcarver@hotmail.com>
2020-07-30 20:26:28 -0500

    modified landingpage

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
+const dev = {
+  STRIPE_KEY: "pk_test_n1sBjALNl7edw^zcbsrYf7GS00UG7GhBaN",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-10dqqj0imhdsy"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://w02p22aknc.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_CoPUtyj2l",
+    APP_CLIENT_ID: "60koq1j4a6d0afnn23tr7tu86",
+    IDENTITY_POOL_ID: "us-east-1:fe0af6c9-890c-4874-a148-24c0203e233c"
+  }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_n1sBjALNl7edw^zcbsrYf7GS00UG7GhBaN",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentbucket-1ueltuktfxhj0"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://cwzveykkf0.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_RHO8YvOHn",
+    APP_CLIENT_ID: "23e6r3nhvf8cjmsptd67dec6rh",
+    IDENTITY_POOL_ID: "us-east-1:fe0af6c9-890c-4874-a148-24c0203e233c"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod' ?
+  prod :
+  dev;
+
 export default {
-   MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "bucketonotes"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "https://fx1w7oolz1.execute-api.us-east-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_KQesvlsjM",
-      APP_CLIENT_ID: "6rs6r0rsuk7uq6npbdnbuqncue",
-      IDENTITY_POOL_ID: "us-east-1:d84a5697-1c00-4908-9434-16e4e11ff5dd"
-    },
-    STRIPE_KEY: "pk_test_n1sBjALNl7edw^zcbsrYf7GS00UG7GhBaN"  
-  };
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import React, {
+  useState,
+  useEffect
+} from "react";
+import {
+  PageHeader,
+  ListGroup,
+  ListGroupItem
+} from "react-bootstrap";
 import "./Home.css";
-import { API } from "aws-amplify";
-import { LinkContainer } from "react-router-bootstrap";
+import {
+  API
+} from "aws-amplify";
+import {
+  LinkContainer
+} from "react-router-bootstrap";
+import {
+  Link
+} from "react-router-dom";
 
 export default function Home(props) {
   const [notes, setNotes] = useState([]);
@@ -13,67 +27,97 @@ export default function Home(props) {
       if (!props.isAuthenticated) {
         return;
       }
-  
+
       try {
         const notes = await loadNotes();
         setNotes(notes);
       } catch (e) {
         alert(e);
       }
-  
+
       setIsLoading(false);
     }
-  
+
     onLoad();
   }, [props.isAuthenticated]);
-  
+
   function loadNotes() {
     return API.get("notes", "/notes");
   }
 
   function renderNotesList(notes) {
     return [{}].concat(notes).map((note, i) =>
-      i !== 0 ? (
-        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
-          <ListGroupItem header={note.content.trim().split("\n")[0]}>
-            {"Created: " + new Date(note.createdAt).toLocaleString()}
-          </ListGroupItem>
-        </LinkContainer>
-      ) : (
-        <LinkContainer key="new" to="/notes/new">
-          <ListGroupItem>
-            <h4>
-              <b>{"\uFF0B"}</b> Create a new note
-            </h4>
-          </ListGroupItem>
-        </LinkContainer>
+      i !== 0 ? ( <
+        LinkContainer key = {
+          note.noteId
+        }
+        to = {
+          `/notes/${note.noteId}`
+        } >
+        <
+        ListGroupItem header = {
+          note.content.trim().split("\n")[0]
+        } > {
+          "Created: " + new Date(note.createdAt).toLocaleString()
+        } <
+        /ListGroupItem> <
+        /LinkContainer>
+      ) : ( <
+        LinkContainer key = "new"
+        to = "/notes/new" >
+        <
+        ListGroupItem >
+        <
+        h4 >
+        <
+        b > {
+          "\uFF0B"
+        } < /b> Create a new note <
+        /h4> <
+        /ListGroupItem> <
+        /LinkContainer>
       )
     );
   }
 
   function renderLander() {
-    return (
-      <div className="lander">
-        <h1>Jason's Notes</h1>
-        <p>A simple note taking app</p>
-      </div>
+    return ( <
+      div className = "lander" >
+      <
+      h1 > Jason 's Notes</h1> <
+      p > A simple note taking app < /p> <
+      div >
+      <
+      Link to = "/login"
+      className = "btn btn-info btn-lg" >
+      Login <
+      /Link> <
+      Link to = "/signup"
+      className = "btn btn-success btn-lg" >
+      Signup <
+      /Link> <
+      /div> <
+      /div>
     );
   }
 
   function renderNotes() {
-    return (
-      <div className="notes">
-        <PageHeader>Jason's Notes</PageHeader>
-        <ListGroup>
-          {!isLoading && renderNotesList(notes)}
-        </ListGroup>
-      </div>
+    return ( <
+      div className = "notes" >
+      <
+      PageHeader > Jason 's Notes</PageHeader> <
+      ListGroup > {
+        !isLoading && renderNotesList(notes)
+      } <
+      /ListGroup> <
+      /div>
     );
   }
 
-  return (
-    <div className="Home">
-      {props.isAuthenticated ? renderNotes() : renderLander()}
-    </div>
+  return ( <
+    div className = "Home" > {
+      props.isAuthenticated ? renderNotes() : renderLander()
+    } <
+    /div>
   );
 }
